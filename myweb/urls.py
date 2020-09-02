6f8e9c7793f0c868a2822aaaa127f16ac0258063
Anupam <anupamsinghj@gmail.com>
2020-09-03 01:43:55 +0530

    02/02

--- myweb/urls.py
+++ myweb/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
-app_name = 'polls'
+from django.views.generic import TemplateView
+app_name = 'myweb'
 
 urlpatterns = [
     path('b', views.bcd),
+    path('ba', views.ba),
+    path('m18', views.m18,name='map')
 ]
--- myweb/views.py
+++ myweb/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 def bcd(request):
     return render(request, "myweb/bcd.html")
+def ba(request):
+    return render(request, "myweb/1.html")
+def m18(request):
+        return render(request, "myweb/map18.html")
